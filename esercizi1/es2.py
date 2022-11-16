3f828c384b55addfee64f06cfdf43c1d205bc538
lorfiaban <115221581+lorfiaban@users.noreply.github.com>
2022-11-16 08:24:17 +0000

    prova1es2

---
+++ esercizi1/es2.py
@@ -0,0 +1,16 @@
+
+def sum_list(list):
+    somma=0
+    for item in(list):
+        somma=somma+item
+    return(somma)
+
+
+list=[2, 5, 4, 7]
+som=sum_list(list)
+print('valore {}'.format(som))
+
+
+
+
+
