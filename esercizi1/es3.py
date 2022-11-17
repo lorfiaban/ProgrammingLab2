a22989b9e472d732a2154b3cdfd92197eff4e587
lorfiaban <115221581+lorfiaban@users.noreply.github.com>
2022-11-17 13:33:24 +0000

    prova2es3

--- esercizi1/es3.py
+++ esercizi1/es3.py
@@ -2,10 +2,10 @@ def sum_csv(file_name):
     sum=0
     for line in file_name:
         elements=line.split(',')
-        if elements[0] != 'date':
-            value=float(elements[0])
-            sum=sum+value
-        else:
-            value=float(elements[1])
-            sum=sum+value
+        #if elements[0] != 'date':
+        value=float(elements[0])
+        sum=sum+value
+       # else:
+        #    value=float(elements[1])
+         #   sum=sum+value
     return(sum)
